249df8c1a7fb2d8db30aba059ee5e3b6d078999f
Kevin Partington <kevin.partington@kernelpanicstudios.com>
2015-05-05 20:16:33 -0500

    First pass at migrate.js file.

---
+++ migrate.js
@@ -0,0 +1,17 @@
+var fs = require("fs");
+var esprima = require("esprima");
+
+function processFile (file) {
+    fs.readFile(file, "utf8", function (err, contents) {
+        if (err) throw err;
+
+        var syntax = esprima.parse(contents);
+        console.log(JSON.stringify(syntax, null, 2));
+    });
+}
+
+module.exports = processFile;
+
+if (require.main === module) {
+    processFile(process.argv[2]);
+}
