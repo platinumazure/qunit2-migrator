ce27e7fc82f805565dae98468a8bcf7692a5fa2a
Kevin Partington <kevin.partington@kernelpanicstudios.com>
2015-05-14 21:49:39 -0500

    Added minimist arg parser and simple argument handling.

--- main.js
+++ main.js
@@ -1,21 +1,51 @@
 var fs = require("fs");
+var minimist = require("minimist");
 var recast = require("recast");
 
 var transform = require("./transform");
 
-function processFile (file) {
-    fs.readFile(file, "utf8", function (err, contents) {
-        if (err) throw err;
+function processFile (argv) {
+    var file = argv._[0];
+    var output = argv.output || "transformedFile";
 
-        var syntax = recast.parse(contents);
-        syntax = transform(syntax);
+    if (file) {
+        fs.readFile(file, "utf8", function (err, contents) {
+            if (err) throw err;
 
-        console.log(recast.print(syntax).code);
-    });
+            var syntax = recast.parse(contents);
+
+            switch (output) {
+                case "syntaxTreePreTransform":
+                    console.log(JSON.stringify(syntax, null, 4));
+                    break;
+
+                case "syntaxTreePostTransform":
+                    syntax = transform(syntax);
+                    console.log(JSON.stringify(syntax, null, 4));
+                    break;
+
+                case "transformedFile":
+                    syntax = transform(syntax);
+                    console.log(recast.print(syntax).code);
+                    break;
+
+                default:
+                    throw new Error("Invalid output mode");
+            }
+        });
+    } else {
+        throw new Error("No file supplied");
+    }
 }
 
 module.exports = processFile;
 
 if (require.main === module) {
-    processFile(process.argv[2]);
+    var argv = minimist(process.argv.slice(2), {
+        default: {
+            output: null
+        }
+    });
+
+    processFile(argv);
 }
