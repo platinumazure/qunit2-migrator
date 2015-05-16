aa5725c96b674d140880899060cc0ae0a217652e
Kevin Partington <kevin.partington@kernelpanicstudios.com>
2015-05-16 11:34:09 -0500

    Ensure safe exit on potentially asynchronous output operation.

--- main.js
+++ main.js
@@ -6,14 +6,18 @@ var transform = require("./transform");
 
 var outputFile;
 
-function writeOutput (contents) {
+function writeOutput (contents, callback) {
     if (outputFile) {
-        fs.writeFile(outputFile, contents, "utf8", function (err, contents) {
-            if (err) throw err;
-        });
+        fs.writeFile(outputFile, contents, "utf8", callback);
     } else {
         console.log(contents);
+        callback();
     }
+}
+
+function onOutput (err) {
+    if (err) throw err;
+    process.exit(0);
 }
 
 function processFile (argv) {
@@ -28,17 +32,17 @@ function processFile (argv) {
 
             switch (argv.outputMode) {
                 case "syntaxTreePreTransform":
-                    writeOutput(JSON.stringify(syntax, null, 4));
+                    writeOutput(JSON.stringify(syntax, null, 4), onOutput);
                     break;
 
                 case "syntaxTreePostTransform":
                     syntax = transform(syntax);
-                    writeOutput(JSON.stringify(syntax, null, 4));
+                    writeOutput(JSON.stringify(syntax, null, 4), onOutput);
                     break;
 
                 case "transformedFile":
                     syntax = transform(syntax);
-                    writeOutput(recast.print(syntax).code);
+                    writeOutput(recast.print(syntax).code, onOutput);
                     break;
 
                 default:
@@ -61,4 +65,9 @@ if (require.main === module) {
     });
 
     processFile(argv);
+
+    // Ensure stdout flushes
+    process.on("exit", function (exitCode) {
+        process.exit(exitCode);
+    });
 }
